6f4f0d582fc556562cf099910da06ce13334f65c
Petter Peterson <petter.e.peterson@gmail.com>
2019-06-18 20:29:21 +0200

    adding files

--- spec/trie_spec.cr
+++ spec/trie_spec.cr
@@ -1,9 +1,216 @@
 require "./spec_helper"
+require "../src/trie.cr"
+
+def words
+  words = %w{Haxx0r ipsum crack endif throw buffer ban ip brute force else big-endian I'm sorry Dave, I'm afraid I can't do that bit. Loop access void wombat cache function ctl-c hello world ack pragma default then stack trace irc while. Printf double gobble spoof fail true loop flood over clock James T. Kirk headers dereference January 1, 1970 /dev/null overflow script kiddies grep break server bubble sort fatal concurrently frack. Xss tcp crack ip loop hexadecimal true gc root stack trace error fopen. Packet sniffer *.* dereference system int if James T. Kirk echo man pages bang worm tunnel in default flood char deadlock Donald Knuth Public Starcraft server mutex default Leslie Lamport I'm sorry Dave, I'm afraid I can't do that. Tcp eof crack tcp mountain dew epoch cache int mainframe ack bang infinite loop memory leak buffer if. Firewall stdio.h private less break ascii else try catch stack thread chown flush flood perl double over clock}
+
+  words.map do |word|
+    word.gsub(/[\.*\/,]/, "")
+  end.reject { |x| x.size == 0 }
+end
 
 describe Trie do
-  # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  context "searching" do
+    it "should load words into a structure and list all"  do
+      trie = Trie.new
+      words.each do |word|
+        trie.add(word.downcase)
+      end
+      res = trie.all
+      res.size.should eq(114)
+      trie.count.should eq(114)
+    end
+
+    it "should find similar" do
+      trie = Trie.new
+      words.each do |word|
+        trie.add(word.downcase)
+      end
+      res = trie.find("sys")
+      res.size.should eq(2)
+      puts res.inspect
+      res = trie.find("yste")
+      res.size.should eq(6)
+
+      words = res.map{|x|x.first}
+      ["system", "stack", "server", "stdioh", "xss", "starcraft"].each do |w|
+        words.includes?(w).should be_true
+      end
+    end
+
+    it "should find buffer" do
+      trie = Trie.new
+      words.each do |word|
+        trie.add(word.downcase)
+      end
+
+      res = trie.find("uuufer")
+      res.size.should eq(1)
+      res.map(&.first).includes?("buffer").should be_true
+
+    end
+
+    it "should find buffer 2" do
+      trie = Trie.new
+      trie.add("buffer")
+
+      res = trie.find("uufer")
+      puts res.inspect
+
+      words = res.map(&.first)
+      words.size.should eq(1)
+      ["buffer"].each do |w|
+        words.includes?(w).should be_true
+      end
+
+      trie = Trie.new
+      trie.add("default")
+      trie.add("devnull")
+
+      res = trie.find("dewnul")
+      words = res.map(&.first)
+      words.size.should eq(1)
+      ["devnull"].each do |w|
+        words.includes?(w).should be_true
+      end
+    end
+
+    it "should support several words in same branch" do
+      trie = Trie.new
+      words = %w{sol soldat solid}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+
+      res = trie.all
+      res.size.should eq(3)
+    end
+
+    it "should find similar items beginning with" do
+      trie = Trie.new
+      words = %w{sol soldat solid}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+
+      res = trie.beginning_with("s")
+      res.size.should eq(3)
+
+      res = trie.beginning_with("sold")
+      res.size.should eq(1)
+
+      res = trie.beginning_with("soly")
+      res.size.should eq(0)
+    end
+
+    it "should accept spelling errors" do
+      trie = Trie.new
+      words = %w{sol soldat solid}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+
+      res = trie.find("sldatt")
+      res.size.should eq(3)
+      res.map(&.first).first.should eq("soldat")
+
+      res = trie.find("soold")
+      res.size.should eq(3)
+      words = res.map(&.first)
+      words.includes?("soldat").should be_true
+      words.includes?("sol").should be_true
+      words.includes?("solid").should be_true
+    end
+
+    it "should find short words too" do
+      trie = Trie.new
+      words = %w{ab nu se}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+      words.size.should eq(3)
+      trie.count.should eq(3)
+
+      res = trie.find("aaab")
+      res.size.should eq(1)
+      res.map(&.first).first.should eq("ab")
+    end
+    
+    it "should find short words too 2" do
+      trie = Trie.new(rwd_max_nomatch_count: 2)
+      words = %w{ab ad ac}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+      trie.count.should eq(3)
+
+      trie = Trie.new(rwd_max_nomatch_count: 2)
+      words = %w{ab ad ac}
+      words.each do |w|
+        trie.add(w.downcase)
+      end
+      res = trie.find("aaab")
+      puts res.inspect
+      res.map(&.first).first.should eq("ab")
+    end
+
+    it "should skip over nonmatching char" do
+      #todo: map likely replacement chars?
+
+      trie = Trie.new(
+        rwd_max_nomatch_count: 2,
+        fwd_max_nomatch_count: 1,
+      )
+      trie.add("bromma")
+      res = trie.find("bråmma")
+      puts res
+      res = trie.find("brååmma")
+      puts res
+    end
+
+    it "should handle large data sets" do
+      trie = Trie.new(
+        fwd_max_nomatch_count: 1,
+        rwd_max_nomatch_count: 1,
+        min_relevance: 0.8, #high value since we have a lot of items
+      )
+      t = Time.now
+      File.each_line(File.join(File.dirname(__FILE__), "fixtures", "swe_wordlist.txt")) do |line|
+        trie.add(line)
+      end
+      puts Time.now - t
+      puts "--"
+
+      t = Time.now
+      res = trie.beginning_with("tram")
+      puts res.inspect
+      puts Time.now - t
+      puts "--"
+
+      t = Time.now
+      res = trie.find("tramsig")
+      puts res.size
+      puts res[0..10].inspect
+      puts Time.now - t
+
+      t = Time.now
+      res = trie.find("a")
+      puts res.size
+      puts res[0..10].inspect
+      puts Time.now - t
+      
+      t = Time.now
+      res = trie.find("mjäcklare")
+      puts res.size
+      puts res[0..10].inspect
+      puts Time.now - t
+      
+      t = Time.now
+      res = trie.find("qwe")
+      puts res.size
+      puts res[0..10].inspect
+      puts Time.now - t
+    end
   end
 end
--- src/trie.cr
+++ src/trie.cr
@@ -1,6 +1,231 @@
-# TODO: Write documentation for `Trie`
-module Trie
-  VERSION = "0.1.0"
+class Trie
+  class Node
+    property parent : Node?
+    property value : Char?
+    property children : Set(Node)
 
-  # TODO: Put your code here
+    def_equals_and_hash @value
+
+    def initialize(parent : Node?, value : Char?)
+      @parent = parent
+      @value = value
+      @children = Set(Node).new
+    end
+
+    # unused
+    def is_root?
+      parent.nil? && value.nil?
+    end
+
+    def is_end?
+      !parent.nil? && value.nil?
+    end
+
+    def add(ord : Array(Char))
+      if ord.size > 0
+        c = ord.delete_at(0)
+        n = Node.new(parent: self, value: c)
+        n1 = @children.find { |i| i == n }
+        if n1.nil?
+          @children.add(n)
+          n.add(ord)
+        else
+          n1.add(ord)
+        end
+      else
+        @children.add(Node.new(parent: self, value: nil))
+      end
+    end
+
+    def to_s
+      res = [] of Char?
+      node = self
+      while(node != nil)
+        n = node.not_nil!
+        res << n.value unless n.value.nil?
+        node = n.parent
+      end
+      res.reverse.join
+    end
+  end
+
+  property root : Node
+  property fwd_max_nomatch_count : Int8
+  property rwd_max_nomatch_count : Int8
+  property min_relevance : Float32 # TODO: rename since it is relevance where we're *not* done
+
+  def initialize(fwd_max_nomatch_count : Int8 = 1,
+                 rwd_max_nomatch_count : Int8 = 1,
+                 min_relevance : Float32 = 0.2)
+    @root = Node.new(parent: nil, value: nil)
+    @fwd_max_nomatch_count = fwd_max_nomatch_count
+    @rwd_max_nomatch_count = rwd_max_nomatch_count
+    @min_relevance = min_relevance # relevance val when done scanning input word n.b
+  end
+
+  def add(word : String)
+    root.add(word.chars)
+  end
+
+  def all
+    res = [] of String
+    root.children.each do |child|
+      collect(child, res)
+    end
+    res
+  end
+
+  def count
+    all.size
+  end
+
+  def beginning_with(word : String)
+    res = [] of String
+    root.children.each do |child|
+      recursive_strict(child, word.chars, 0, res)
+    end
+    res
+  end
+
+  def find(word : String)
+
+    res = {} of String => Float64
+
+    root.children.each do |child|
+      recursive_lenient(child,
+                        word.chars,
+                        0,
+                        0,
+                        0,
+                        0,
+                        res)
+    end
+    res.to_a.sort_by do |value, relevance|
+      relevance
+    end.reverse
+  end
+
+  private def collect(node : Node, 
+                      result : Enumerable(String))
+
+    node.children.each do |child|
+      collect(child, result)
+    end
+    if node.is_end?
+      result << node.to_s
+    end
+  end
+
+  private def recursive_strict(node : Node,
+                               word : Array(Char),
+                               position : Int,
+                               result : Array(String))
+
+    if word.size > 0 && position < word.size &&
+        if node.value != word[position]
+          return
+    end
+    end
+    node.children.each do |child|
+      recursive_strict(child, word, position + 1, result)
+    end
+    if node.is_end?
+      result << node.to_s
+    end
+  end
+
+  private def recursive_lenient(node : Node,
+                                word : Array(Char),
+                                position : Int,
+                                match_count : Int,
+                                fwd_count : Int,
+                                rwd_count : Int,
+                                result : Hash(String, Float))
+
+    if node.is_end?
+      s = node.to_s
+
+      if match_count > 0
+
+        relevance = match_count.to_f/s.size.to_f
+        # puts "#{s} - match count: #{match_count}, " +
+        #      "relevance: #{relevance}"
+
+        # TODO: (de)increase relevance if word[0] == (!=)  s.first ?
+        # TODO: 
+
+        if s.size < word.size
+          #TEST (lower short word ranking)
+          #increase this penalty
+          relevance = relevance * (s.size.to_f/word.size.to_f)
+        end
+
+        if result.has_key?(s)
+          if result[s] < relevance
+            result[s] = relevance
+          end
+        else
+          result[s] = relevance
+        end
+      end
+      return
+    end
+
+    if position >= word.size
+      # just collect the rest without any adjustments etc
+
+      if match_count > 0
+        # on very large sets we need to limit response size
+        s = node.to_s #collect what we got so far
+        relevance = match_count.to_f/s.size.to_f
+
+        if relevance > min_relevance
+          node.children.each do |child|
+            recursive_lenient(child,
+                              word,
+                              position,
+                              match_count,
+                              0,
+                              0,
+                              result)
+          end
+        end
+      end
+      return
+    end
+
+    if node.value == word[position]
+      node.children.each do |child|
+        recursive_lenient(child,
+                          word,
+                          position + 1,
+                          match_count + 1,
+                          0,
+                          0,
+                          result)
+      end
+    else
+      if fwd_count < fwd_max_nomatch_count && node.value != nil
+        node.children.each do |child|
+          recursive_lenient(child,
+                            word,
+                            position,
+                            match_count,
+                            fwd_count + 1,
+                            rwd_count,
+                            result)
+        end
+      end
+
+      if rwd_count < rwd_max_nomatch_count && node.value != nil
+        recursive_lenient(node,
+                          word,
+                          position + 1,
+                          match_count,
+                          fwd_count,
+                          rwd_count + 1,
+                          result)
+      end
+    end
+  end
 end
